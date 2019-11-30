ca256650edf0c5dbba02f8a62cab05cf4c3f2f0e
18217777392 <54964519+18217777392@users.noreply.github.com>
2019-11-30 23:47:57 +0800

    Add files via upload

---
+++ Life Clock_blueprint.py
@@ -0,0 +1,69 @@
+import datetime
+import math
+
+def front_end():
+
+     
+
+def core():
+
+        '/get users information/'
+        
+        
+        '/get system information/'
+        
+        
+        '/calculate day in age /'
+        
+        
+        '/return age and print day in age/'
+        
+        return age
+
+def suggest(Age):
+
+     '/According to the Wikipedia, today, the average life expectancy of humans has reached *79* years old./'
+ 
+          
+     '/for those who have exceeded the average human lifespan/'
+     
+     if Age>79:
+
+     '/for boys and girls/'
+     
+     if Age<14:
+     
+     
+     if (Age>13 and Age<81):
+        
+          '/for teenagers/'
+          
+          if Age<30:
+          
+          '/for elderly/'
+          
+          if Age>60:
+             
+          '/for middle-aged/'
+          
+          if (Age<61 and Age>29):
+             
+     '/return 2020 goal and print suggest/'
+             
+     return Goal
+
+def output(Age,str):
+
+     if (Age>13 and Age<80):
+     
+          '/It is December,2019/'
+          '/January-November:365-31=334/'
+     
+def main():
+
+     front_end()
+     age=core()   
+     users_goal=suggest(age)       
+     output(age,users_goal)
+
+main()  
